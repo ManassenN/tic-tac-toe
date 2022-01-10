13b0975b79eb6fcafb11228085b0d9a7794e19f4
Nerya Manassen <neryamanassen9@gmail.com>
2022-01-10 21:27:46 +0200

    Minor changes

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 game_board = {'7': ' ', '8': ' ', '9': ' ',
             '4': ' ', '5': ' ', '6': ' ',
             '1': ' ', '2': ' ', '3': ' '}
-
+#Global variables
+game = True
 board_is_full = False
 turn = True
 empty = ' '
-game = True
+
+
 # Print the current status of the board
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
@@ -15,6 +17,24 @@ def printBoard(board):
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-----')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+def play_again(board):
+    question = input("Do You Want To Play Again? (Y/N): ")
+    if question == 'Y':
+        board = clean_board(board)
+        play(board)
+    elif question == 'N':
+        print("GOODBYE!")
+        quit()
+    else:
+        print("Try Again")
+        play_again(board)
+
+def clean_board(board):
+    for i in range(1,10):
+        board[f"{i}"] = ' '
+
+    return board
 
 def square_is_taken(board,square_choice):
     if board[square_choice] != ' ':
@@ -24,21 +44,24 @@ def square_is_taken(board,square_choice):
         return True
 
 
-
+# Checking if there is any wins on the horizontal columns.
 def check_horizontal_win(board):
     for i in range(1,8,3):
         if board[f"{i}"] == board[f"{i+1}"] == board[f"{i+2}"]!=empty :
             potential_winner = board[f"{i}"]
             print(f'Game Over {potential_winner} won the game!')
-            game = False
+            play_again(board)
 
+# Checking if there is any wins on the vertical columns.
 def check_vertical_win(board):
     for i in range(1,3):
         if board[f"{i}"] == board[f"{i+3}"] == board[f"{i+6}"]!=empty:
             potential_winner = board[f"{i}"]
             print(f'Game Over {potential_winner} won the game!')
-            game = False
+            play_again(board)
 
+
+# Checking if there is any wins on the diagonals
 def check_diagonal_win(board):
     potential_winner = board["1"]
     if board["1"] == board["5"] == board["9"] !=empty :
@@ -47,7 +70,8 @@ def check_diagonal_win(board):
     elif board["3"] == board["5"] == board["7"]!=empty :
         potential_winner = board["3"]
         print(f'Game Over {potential_winner} won the game!')
-        game = False
+
+
 
 def check_board_status(board):
     check_diagonal_win(board)
@@ -55,9 +79,9 @@ def check_board_status(board):
     check_vertical_win(board)
 
 def play(board,turn):
-    while board_is_full==False and game:
+    while board_is_full==False :
         square_choice = input()
-        if turn ==True:
+        if turn:
             print("it's O turn!")
             if square_is_taken(board,square_choice):
                 board[square_choice] = 'O'
